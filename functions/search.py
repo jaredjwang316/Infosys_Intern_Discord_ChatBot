0ca9cc2e22f145a35f0e8e004ec3928320d52197
jaredjwang316 <jwangj001@tamu.edu>
2025-06-19 12:41:23 -0500

    Used pgvector for semantic searching of a discord chat

--- functions/search.py
+++ functions/search.py
@@ -26,14 +26,13 @@ model = ChatGoogleGenerativeAI(
     max_retries=2
 )
 
+# takes text (like "Hi how are you") and returns a high-dimensional vector that represents the semantic meaning.
 embedding_model = GoogleGenerativeAIEmbeddings(
     model="models/text-embedding-004"
 )
 
-# Connection string for pgvector
 connection_string = f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
-# uses Chroma, FAISS is hard to install on macos - rochan
 def search_conversation(history, search_query):
     # turn history into Documents
     docs = [
@@ -45,10 +44,17 @@ def search_conversation(history, search_query):
     splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     chunks = splitter.split_documents(docs)
 
-    #Each chunk is embedded into a vector using Gemini’s embedding model
-    #The resulting vectors are stored in a local vector index using Annoy (a fast similarity search library)
-    #This turns text into math, allowing us to search by meaning instead of just keywords.
-    vectorstore = Annoy.from_documents(chunks, embedding_model, index_params={"n_trees": 10})
+    # 3. Store in pgvector 
+    # The vectors and associated text data will be stored in the chat_embeddings table created in my schema
+    vectorstore = PGVector(
+        collection_name="chat_embeddings",  #specifies the table name in your PostgreSQL database where the vectors and associated text data will be stored.
+        connection_string=connection_string,
+        embedding_function=embedding_model,     #convert your input text into semantic vector embeddings 
+        distance_strategy="cosine"  #cosine measures how similar vectors are directionally, not in terms of magnitude 
+    )
+
+    # add_documents only once (or check if already in DB)
+    vectorstore.add_documents(chunks)
 
     # build a RetrievalQA chain
     qa = RetrievalQA.from_chain_type(
